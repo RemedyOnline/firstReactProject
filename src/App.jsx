867f5a56f82bc2591c0cab3e001389fa43c48617
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-09-30 16:22:32 +0000

    DAY 1

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  
+  return(
+  <section>
+    <header>
+    <nav>
+      <a href="#">LOGO</a>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">Campuses</a></li>
+        <li><a href="#">Hostels</a></li>
+        <li><a href="#">About Us</a></li>
+      </ul>
+    </nav>
+    </header>
+    <main className='mainContent'>
+    <h1>Weclome to Mountain Afadjato!</h1>
+  </main>
+  </section>
+  
+  );
+}
+
+export default App
